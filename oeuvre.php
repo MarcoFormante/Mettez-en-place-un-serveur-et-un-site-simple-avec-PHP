4427ed3daedd2598dc2d070e6fb5837ae8890181
Marco <formante.marco@gmail.com>
2025-09-27 12:33:47 +0200

    Add database connection, collect oeuvre data and escape all fields

--- oeuvre.php
+++ oeuvre.php
@@ -1,38 +1,37 @@
 <?php
     require 'header.php';
-    require 'oeuvres.php';
+
+    require 'functions.php';
+    require 'bdd.php';
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
-        header('Location: index.php');
+        redirectTo('index');
     }
 
-    $oeuvre = null;
+    // On recuper l'oeuvre depuis le database
+    $pdoConnection = connection();
+    $stmt = $pdoConnection->prepare("SELECT * FROM oeuvres WHERE id = :id");
+    $stmt->execute(['id' => $_GET['id']]);
+  
+    $oeuvre = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
-
+    
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
+    if(!$oeuvre) {
+        redirectTo('index');
     }
 ?>
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= htmlspecialchars($oeuvre['image']) ?>" alt="<?= htmlspecialchars($oeuvre['titre']) ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?= htmlspecialchars($oeuvre['titre']) ?></h1>
+        <p class="description"><?= htmlspecialchars($oeuvre['artiste']) ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+             <?= nl2br(htmlspecialchars($oeuvre['description'])) ?>
         </p>
     </div>
 </article>
