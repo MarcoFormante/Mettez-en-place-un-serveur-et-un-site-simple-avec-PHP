81b4a0b69d3a0ffdab6a4a300271a7e453dc286e
Marco <formante.marco@gmail.com>
2025-09-27 12:56:02 +0200

    inputs validation

---
+++ traitement.php
@@ -0,0 +1,37 @@
+<?php 
+require 'functions.php';
+require 'bdd.php';
+
+
+$errors = [];
+$FORM_PARAMS = ['titre', 'artiste', 'image', 'description'];
+
+foreach ($FORM_PARAMS as $param) {
+    // check if fields are set
+    if (!isset($_POST[$param])) {
+        redirectTo('ajouter');
+    }
+
+    // check if fields are empty
+    if(empty($_POST[$param]) || trim($_POST[$param]) === "" ){  
+        $errors[] = "Le champ <b>". strtoupper($param) ."</b> ne peut pas etre vide";
+    }
+}
+
+
+// check if description field has at least 3 characters
+if (strlen($_POST['description']) < 3) {
+    $errors[] = "Le champ <b> DESCRIPTION </b> doit contenir au-moins 3 caracter";
+}
+   
+
+    // check if is a valid URL  
+if( !filter_var($_POST['image'],FILTER_VALIDATE_URL) || !preg_match('/^https?:\/\/.+\.(jpg|jpeg|png|gif|webp)$/i', $_POST['image']) ) {
+    $errors[] = "Le champ <b> IMAGE </b> doit avoir un URL valide : <br/> <b>Exemple</b>: https://chemin/image.( jpg | jpeg | png | gif )";
+}
+
+
+if (!count($errors)) {
+
+    echo "HELLO WORLD";
+}
