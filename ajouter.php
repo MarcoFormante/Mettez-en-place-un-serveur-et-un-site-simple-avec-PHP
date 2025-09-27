fa8b28c4de07a8ad92fd5a7cb6bacd9bbdc56daa
Marco <formante.marco@gmail.com>
2025-09-27 12:46:19 +0200

    add 'required' for all form inputs

--- ajouter.php
+++ ajouter.php
@@ -3,19 +3,19 @@
 <form action="traitement.php" method="POST">
     <div class="champ-formulaire">
         <label for="titre">Titre de l'œuvre</label>
-        <input type="text" name="titre" id="titre">
+        <input required type="text" name="titre" id="titre">
     </div>
     <div class="champ-formulaire">
         <label for="artiste">Auteur de l'œuvre</label>
-        <input type="text" name="artiste" id="artiste">
+        <input required type="text" name="artiste" id="artiste">
     </div>
     <div class="champ-formulaire">
         <label for="image">URL de l'image</label>
-        <input type="url" name="image" id="image">
+        <input required type="url" name="image" id="image">
     </div>
     <div class="champ-formulaire">
         <label for="description">Description</label>
-        <textarea name="description" id="description"></textarea>
+        <textarea required name="description" id="description"></textarea>
     </div>
 
     <input type="submit" value="Valider" name="submit">
